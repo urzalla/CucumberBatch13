e3e75b336a112047c441d4d9df5a205abbefd315
sohai <SyntaxTechnologies>
2022-08-14 00:03:46 +0530

    lc1308

--- src/test/java/runners/RunnerClass.java
+++ src/test/java/runners/RunnerClass.java
@@ -8,8 +8,12 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         //features we use to provide the path of all the features file
-    features = "src/test/resources/features/Login.feature"
+        features = "src/test/resources/features/Login.feature",
+        //glue is where we find implementations for gherkin steps
+        //we provide the path of package where we defined all the steps
+        glue = "steps"
 )
 public class RunnerClass {
+
 }
 
--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -3,14 +3,20 @@ package steps;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginSteps {
 
+    public static WebDriver driver;
+
     @Given("user is navigated to HRMS application")
     public void user_is_navigated_to_hrms_application() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
     }
+
     @When("user enters valid admin username and password")
     public void user_enters_valid_admin_username_and_password() {
         // Write code here that turns the phrase above into concrete actions
