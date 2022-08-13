916d520559c8628ad7ac4cc3bd54bd28424c9f03
sohai <SyntaxTechnologies>
2022-08-13 23:20:47 +0530

    lc1308

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -16,16 +16,16 @@ public class LoginSteps {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
+
     @When("user clicks on login button")
     public void user_clicks_on_login_button() {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
+
     @Then("admin user is successfully logged in")
     public void admin_user_is_successfully_logged_in() {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
-
-
 }
